b732c9c5db2b2146f4c1abd9caebce07ab8b96e5
James Wickenden <jameswickenden14@gmail.com>
2020-05-23 00:17:09 +0100

    toggleable personal about editor

--- site/public/resources/scripts/handle_home.js
+++ site/public/resources/scripts/handle_home.js
@@ -1,11 +1,12 @@
 "use strict";
 
 addEventListener('load', loadHomeSession);
-
+var user_id;
+var desc_editor;
 async function loadHomeSession() {
-  let user_id = parseInt(window.location.pathname.split("/").pop());
-  if (window.location.pathname == "/home"){
 
+  user_id = parseInt(window.location.pathname.split("/").pop());
+  if (window.location.pathname == "/home"){
     user_id = sessionStorage.getItem("user_id");
   };
 
@@ -24,4 +25,26 @@ async function loadHomeSession() {
 
 async function receiveHome(response) {
   document.getElementById("userpage").innerHTML = response.responseText;
+  formatDesc(user_id);
+};
+
+function formatDesc() {
+  if (user_id === null || user_id == "-1") return;
+  if (user_id != sessionStorage.getItem("user_id")) return;
+  desc_editor = document.getElementById("edit_desc");
+
+  document.getElementById("toggle_desc").style = "display:inline";
+  document.getElementById("toggle_desc").addEventListener('click', toggleDesc);
+};
+
+function toggleDesc(event) {
+  event.preventDefault();
+
+  if (desc_editor.style.display == "inline") {
+
+    desc_editor.style = "display:none";
+  }
+  else {
+    desc_editor.style = "display:inline";
+  };
 };
